7948cbe251951699a34f62b5e5687e365866c697
samatrhea <s.gerene@rheagroup.com>
2019-04-23 17:26:38 +0200

    [Fix] unit test refactor Source1 and Source2 to SourceY and SoureX

--- RelationshipMatrix.Tests/ViewModel/RelationshipMatrixViewModelTestFixture.cs
+++ RelationshipMatrix.Tests/ViewModel/RelationshipMatrixViewModelTestFixture.cs
@@ -35,54 +35,54 @@ namespace CDP4RelationshipMatrix.Tests.ViewModel
                 this.dialogNavigationService.Object,
                 this.pluginService.Object);
 
-            Assert.IsFalse(vm.CanEditSource1);
-            Assert.IsFalse(vm.CanEditSource2);
-            Assert.IsFalse(vm.CanInspectSource1);
-            Assert.IsFalse(vm.CanInspectSource2);
+            Assert.IsFalse(vm.CanEditSourceY);
+            Assert.IsFalse(vm.CanEditSourceX);
+            Assert.IsFalse(vm.CanInspectSourceY);
+            Assert.IsFalse(vm.CanInspectSourceX);
 
             Assert.AreEqual(this.participant, vm.ActiveParticipant);
             Assert.AreEqual($"{this.domain.Name} [{this.domain.ShortName}]", vm.DomainOfExpertise);
 
-            Assert.AreEqual(this.settings.PossibleClassKinds.Count, vm.Source1Configuration.PossibleClassKinds.Count);
+            Assert.AreEqual(this.settings.PossibleClassKinds.Count, vm.SourceYConfiguration.PossibleClassKinds.Count);
             Assert.AreEqual(0, vm.RelationshipConfiguration.PossibleRules.Count);
 
-            vm.Source1Configuration.SelectedClassKind = vm.Source1Configuration.PossibleClassKinds.First(x => x == ClassKind.ElementDefinition);
-            vm.Source2Configuration.SelectedClassKind = vm.Source2Configuration.PossibleClassKinds.First(x => x == ClassKind.ElementDefinition);
+            vm.SourceYConfiguration.SelectedClassKind = vm.SourceYConfiguration.PossibleClassKinds.First(x => x == ClassKind.ElementDefinition);
+            vm.SourceXConfiguration.SelectedClassKind = vm.SourceXConfiguration.PossibleClassKinds.First(x => x == ClassKind.ElementDefinition);
 
-            Assert.AreEqual(this.srdl.DefinedCategory.Count(x => x.PermissibleClass.Contains(ClassKind.ElementDefinition)), vm.Source1Configuration.PossibleCategories.Count);
-            Assert.AreEqual(this.srdl.DefinedCategory.Count(x => x.PermissibleClass.Contains(ClassKind.ElementDefinition)), vm.Source2Configuration.PossibleCategories.Count);
+            Assert.AreEqual(this.srdl.DefinedCategory.Count(x => x.PermissibleClass.Contains(ClassKind.ElementDefinition)), vm.SourceYConfiguration.PossibleCategories.Count);
+            Assert.AreEqual(this.srdl.DefinedCategory.Count(x => x.PermissibleClass.Contains(ClassKind.ElementDefinition)), vm.SourceXConfiguration.PossibleCategories.Count);
 
             Assert.AreEqual(this.srdl.Rule.Count, vm.RelationshipConfiguration.PossibleRules.Count);
             Assert.IsEmpty(vm.Matrix.Records);
             Assert.IsEmpty(vm.Matrix.Columns);
 
-            vm.Source1Configuration.SelectedCategories = new List<Category>(vm.Source1Configuration.PossibleCategories.Where(x => x.Iid == this.catEd1.Iid || x.Iid == this.catEd2.Iid));
-            vm.Source2Configuration.SelectedCategories = new List<Category>(vm.Source2Configuration.PossibleCategories.Where(x => x.Iid == this.catEd2.Iid));
-            vm.Source1Configuration.SelectedBooleanOperatorKind = CategoryBooleanOperatorKind.OR;
+            vm.SourceYConfiguration.SelectedCategories = new List<Category>(vm.SourceYConfiguration.PossibleCategories.Where(x => x.Iid == this.catEd1.Iid || x.Iid == this.catEd2.Iid));
+            vm.SourceXConfiguration.SelectedCategories = new List<Category>(vm.SourceXConfiguration.PossibleCategories.Where(x => x.Iid == this.catEd2.Iid));
+            vm.SourceYConfiguration.SelectedBooleanOperatorKind = CategoryBooleanOperatorKind.OR;
             vm.RelationshipConfiguration.SelectedRule = vm.RelationshipConfiguration.PossibleRules.Single();
 
-            vm.Source1Configuration.IncludeSubctegories = false;
+            vm.SourceYConfiguration.IncludeSubctegories = false;
 
             Assert.AreEqual(3, vm.Matrix.Records.Count);
             Assert.AreEqual(3, vm.Matrix.Columns.Count);
 
-            vm.Source1Configuration.IncludeSubctegories = true;
+            vm.SourceYConfiguration.IncludeSubctegories = true;
             
             Assert.AreEqual(5, vm.Matrix.Records.Count);
             Assert.AreEqual(3, vm.Matrix.Columns.Count);
 
-            vm.Source1Configuration.SelectedCategories = new List<Category>(vm.Source1Configuration.PossibleCategories.Where(x => x.Iid == this.catEd3.Iid));
+            vm.SourceYConfiguration.SelectedCategories = new List<Category>(vm.SourceYConfiguration.PossibleCategories.Where(x => x.Iid == this.catEd3.Iid));
 
             Assert.AreEqual(2, vm.Matrix.Records.Count);
             Assert.AreEqual(3, vm.Matrix.Columns.Count);
 
-            vm.Source1Configuration.SelectedCategories = new List<Category>(vm.Source1Configuration.PossibleCategories.Where(x => x.Iid == this.catEd4.Iid));
+            vm.SourceYConfiguration.SelectedCategories = new List<Category>(vm.SourceYConfiguration.PossibleCategories.Where(x => x.Iid == this.catEd4.Iid));
 
             Assert.AreEqual(1, vm.Matrix.Records.Count);
             Assert.AreEqual(3, vm.Matrix.Columns.Count);
 
-            vm.Source1Configuration.SelectedCategories = new List<Category>(vm.Source1Configuration.PossibleCategories.Where(x => x.Iid == this.catEd1.Iid || x.Iid == this.catEd2.Iid));
-            vm.Source1Configuration.SelectedBooleanOperatorKind = CategoryBooleanOperatorKind.AND;
+            vm.SourceYConfiguration.SelectedCategories = new List<Category>(vm.SourceYConfiguration.PossibleCategories.Where(x => x.Iid == this.catEd1.Iid || x.Iid == this.catEd2.Iid));
+            vm.SourceYConfiguration.SelectedBooleanOperatorKind = CategoryBooleanOperatorKind.AND;
 
             Assert.AreEqual(1, vm.Matrix.Records.Count);
             Assert.AreEqual(3, vm.Matrix.Columns.Count);
