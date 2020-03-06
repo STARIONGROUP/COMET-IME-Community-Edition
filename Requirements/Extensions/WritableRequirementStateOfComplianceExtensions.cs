2ec708338482b1aefe0a94c522436c332c07d697
lxatrhea <a.vandelft@rheagroup.com>
2020-03-06 11:59:02 +0100

    Finetuning

--- Requirements/Extensions/WritableRequirementStateOfComplianceExtensions.cs
+++ Requirements/Extensions/WritableRequirementStateOfComplianceExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// ------------------------------------------------------------------------------------------------
 // <copyright file="WritableRequirementStateOfComplianceMethods.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015-2019 RHEA System S.A.
+//   Copyright (c) 2015-2020 RHEA System S.A.
 // </copyright>
 // -----------------------------------------------------------------------------------------------
 
@@ -9,7 +9,6 @@ namespace CDP4Requirements.Extensions
     using System;
     using System.Collections.Generic;
     using System.Reactive.Linq;
-    using System.Threading.Tasks;
 
     using CDP4Common.CommonData;
     using CDP4Common.EngineeringModelData;
