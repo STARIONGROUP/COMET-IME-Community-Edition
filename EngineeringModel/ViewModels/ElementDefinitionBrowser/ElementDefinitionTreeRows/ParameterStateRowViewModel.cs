58c4fe573933a96c2ca475d7807c40e2f43615cd
Kamil Wojnowski <k.wojnowski@rheagroup.com>
2020-03-10 14:22:01 +0100

    ElementDefinitionBrowser an ParameterStateRowViewModel updated

--- EngineeringModel/ViewModels/ElementDefinitionBrowser/ElementDefinitionTreeRows/ParameterStateRowViewModel.cs
+++ EngineeringModel/ViewModels/ElementDefinitionBrowser/ElementDefinitionTreeRows/ParameterStateRowViewModel.cs
@@ -21,6 +21,11 @@ namespace CDP4EngineeringModel.ViewModels
     public class ParameterStateRowViewModel : ParameterValueBaseRowViewModel
     {
         /// <summary>
+        /// Backing field for <see cref="IsDefault"/>
+        /// </summary>
+        
+        private bool isDefault;
+        /// <summary>
         /// Initializes a new instance of the <see cref="ParameterStateRowViewModel"/> class
         /// </summary>
         /// <param name="parameterBase">The associated <see cref="ParameterBase"/></param>
@@ -34,6 +39,7 @@ namespace CDP4EngineeringModel.ViewModels
         {
             this.Name = this.ActualState.Name;
             this.State = this.ActualState.Name;
+            this.IsDefault = this.ActualState.IsDefault;
             this.Option = this.ActualOption;
 
             foreach (var possibleFiniteState in this.ActualState.PossibleState)
@@ -44,6 +50,15 @@ namespace CDP4EngineeringModel.ViewModels
                                    .Subscribe(x => { this.Name = this.ActualState.Name; });
                 this.Disposables.Add(stateListener);
             }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="PossibleFiniteState"/> is the default value of the <see cref="PossibleFiniteStateList"/>
+        /// </summary>
+        public bool IsDefault
+        {
+            get { return this.isDefault; }
+            set { this.RaiseAndSetIfChanged(ref this.isDefault, value); }
         }
     }
 }
