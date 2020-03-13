bc765640025aa81dd37ec4a62274544e75c057b4
lxatrhea <a.vandelft@rheagroup.com>
2020-03-13 16:31:46 +0100

    Fixed installer. Dashboard can also be installed now.

--- CDP4Dashboard/Properties/AssemblyInfo.cs
+++ CDP4Dashboard/Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-using DevExpress.DataAccess.ObjectBinding;
-
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -14,7 +12,6 @@ using DevExpress.DataAccess.ObjectBinding;
 [assembly: AssemblyCopyright("Copyright © RHEA System S.A.")]
 [assembly: AssemblyTrademark("CDP")]
 [assembly: AssemblyCulture("")]
-[assembly: HighlightedAssembly]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
