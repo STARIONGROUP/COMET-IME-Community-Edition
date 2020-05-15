e9257434f26e9fcf2daa43df6269d4a69d3ab56c
nathanatrhea <61700723+nathanatrhea@users.noreply.github.com>
2020-05-15 11:24:24 +0200

    The style of the header of the Product Tree is not alligned with the rest of the panels; fixes #328 (#416)
    * Product tree header updated; fixes #328
    
    Product tree header updated
    BrowserHeader Option field added; fixes #328
    
    * Product tree header updated; fixes #328
    
    - DependencyProperty removed from product tree VM
    - DependencyProperty added on BrowserHeader; fixes #328

--- CDP4Composition/Views/BrowserHeader.xaml.cs
+++ CDP4Composition/Views/BrowserHeader.xaml.cs
@@ -1,11 +1,32 @@
-﻿// -------------------------------------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="BrowserHeader.xaml.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015 RHEA System S.A.
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Merlin Bieze, Naron Phou, Patxi Ozkoidi, Alexander van Delft,
+//            Nathanael Smiechowski, Kamil Wojnowski
+//
+//    This file is part of CDP4-IME Community Edition. 
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
-// -------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CDP4Composition.Views
 {
+    using System.Windows;
     using System.Windows.Controls;
 
     /// <summary>
@@ -14,11 +35,25 @@ namespace CDP4Composition.Views
     public partial class BrowserHeader : UserControl
     {
         /// <summary>
+        /// The dependency property whether the header sould display the option title and its value 
+        /// </summary>
+        public static readonly DependencyProperty IsOptionDependantProperty = DependencyProperty.Register("IsOptionDependant", typeof(bool), typeof(BrowserHeader), new PropertyMetadata(false));
+        
+        /// <summary>
         /// Initializes a new instance of the <see cref="BrowserHeader"/> class.
         /// </summary>
         public BrowserHeader()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="IsOptionDependant"/> dependency property.
+        /// </summary>
+        public bool IsOptionDependant
+        {
+            get => (bool)this.GetValue(IsOptionDependantProperty);
+            set => this.SetValue(IsOptionDependantProperty, value);
         }
     }
 }
