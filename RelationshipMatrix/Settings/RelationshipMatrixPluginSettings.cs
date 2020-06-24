bae15f6172a2cd273b9d58ef5b0c8d9f8c7d3e6f
samatrhea <s.gerene@rheagroup.com>
2020-06-24 16:45:03 +0200

    [Fix] SQ reported vulnerability - https://sonarcloud.io/project/issues?id=RHEAGROUP_CDP4-IME-Community-Edition&issues=AXLf-5M0hwYqv4dMuZIJ&open=AXLf-5M0hwYqv4dMuZIJ

--- RelationshipMatrix/Settings/RelationshipMatrixPluginSettings.cs
+++ RelationshipMatrix/Settings/RelationshipMatrixPluginSettings.cs
@@ -1,17 +1,39 @@
-﻿// -------------------------------------------------------------------------------------------------
-// <copyright file="RelationshipMatrixSettings.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015-2019 RHEA System S.A.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RelationshipMatrixPluginSettings.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft
+//
+//    This file is part of CDP4-IME Community Edition. 
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
-// -------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CDP4RelationshipMatrix
 {
     using System.Collections.Generic;
+    using System.Linq;
+
     using CDP4Common.CommonData;
     using CDP4Composition.PluginSettingService;
-    using CDP4Composition.Services;
+
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
+
     using Settings;
 
     /// <summary>
@@ -22,7 +44,7 @@ namespace CDP4RelationshipMatrix
         /// <summary>
         /// A set of default possible <see cref="ClassKind"/>.
         /// </summary>
-        public static List<ClassKind> DefaultClassKinds = new List<ClassKind>
+        public static IEnumerable<ClassKind> DefaultClassKinds = new List<ClassKind>
         {
             ClassKind.ElementDefinition,
             ClassKind.ElementUsage,
@@ -38,7 +60,7 @@ namespace CDP4RelationshipMatrix
         /// <summary>
         /// A set of default possible <see cref="DisplayKind"/>.
         /// </summary>
-        public static List<DisplayKind> DefaultDisplayKinds = new List<DisplayKind>
+        public static IEnumerable<DisplayKind> DefaultDisplayKinds = new List<DisplayKind>
         {
             DisplayKind.Name,
             DisplayKind.ShortName
@@ -51,8 +73,8 @@ namespace CDP4RelationshipMatrix
         {
             if (initializeDefaults)
             {
-                this.PossibleClassKinds = DefaultClassKinds;
-                this.PossibleDisplayKinds = DefaultDisplayKinds;
+                this.PossibleClassKinds = DefaultClassKinds.ToList();
+                this.PossibleDisplayKinds = DefaultDisplayKinds.ToList();
             }
             else
             {
