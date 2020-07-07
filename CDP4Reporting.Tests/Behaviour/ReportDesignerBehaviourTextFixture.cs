cb1d061afac3b3931e3523312398c85093d5cab7
Adrian Chivu <adrian.chivu@enea.com>
2020-07-07 10:09:50 +0300

    Add tests for ReportDesignerBeahviour.

---
+++ CDP4Reporting.Tests/Behaviour/ReportDesignerBehaviourTextFixture.cs
@@ -0,0 +1,42 @@
+﻿using CDP4Reporting.Behaviours;
+using NUnit.Framework;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CDP4Reporting.Tests.Behaviour
+{
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    public class ReportDesignerBehaviourTextFixture
+    {
+        private ReportDesignerBehaviour reportDesignerBehaviour;
+
+        [SetUp]
+        public void SetUp()
+        {
+            RxApp.MainThreadScheduler = Scheduler.CurrentThread;
+
+            this.reportDesignerBehaviour = new ReportDesignerBehaviour();
+        }
+
+        [Test]
+        public void VerifyTheDefaultValueOfTheDependencyPropertyContainer()
+        {
+            Assert.AreEqual(ReportDesignerBehaviour.RibbonMergeCategoryNameProperty.Name, "RibbonMergeCategoryName");
+        }
+
+        [Test]
+        public void VerifyGetSetRibbonMergeCategoryName()
+        {
+            Assert.IsNull(reportDesignerBehaviour.RibbonMergeCategoryName);
+            reportDesignerBehaviour.RibbonMergeCategoryName = "abc";
+            Assert.AreEqual(reportDesignerBehaviour.RibbonMergeCategoryName, "abc");
+        }
+    }
+}
