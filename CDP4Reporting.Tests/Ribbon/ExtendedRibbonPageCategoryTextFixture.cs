768716faebebd506791e3e578c95b9c1868818f2
Adrian Chivu <adrian.chivu@enea.com>
2020-07-07 07:18:37 +0300

    Add tests for ExtendedRibbonPageCategory ribbon control.

---
+++ CDP4Reporting.Tests/Ribbon/ExtendedRibbonPageCategoryTextFixture.cs
@@ -0,0 +1,46 @@
+﻿using CDP4Dal;
+using NUnit.Framework;
+using ReactiveUI;
+using System.Reactive.Concurrency;
+using System.Threading;
+using CDP4Composition.Ribbon;
+
+namespace CDP4Reporting.Tests.Behaviour
+{
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    public class ExtendedRibbonPageCategoryTextFixture
+    {
+        private ExtendedRibbonPageCategory extendedRibbonPageCategory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            RxApp.MainThreadScheduler = Scheduler.CurrentThread;
+
+            this.extendedRibbonPageCategory = new ExtendedRibbonPageCategory();
+        }
+
+        [Test]
+        public void VerifyTheDefaultValueOfTheDependencyPropertyContainer()
+        {
+            Assert.AreEqual(ExtendedRibbonPageCategory.ContainerRegionNameProperty.Name, "ContainerRegionName");
+        }
+
+        [Test]
+        public void GetSetTheDefaultValueOfTheDependencyPropertyContainer()
+        {
+            Assert.IsNull(extendedRibbonPageCategory.ContainerRegionName);
+
+            extendedRibbonPageCategory.ContainerRegionName = "abc";
+
+            Assert.AreEqual(extendedRibbonPageCategory.ContainerRegionName, "abc");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CDPMessageBus.Current.ClearSubscriptions();
+        }
+    }
+}
