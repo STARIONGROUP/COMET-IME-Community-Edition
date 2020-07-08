106d316084cb0f77b627aadd71df753cc23ceac7
Adrian Chivu <adrian.chivu@enea.com>
2020-07-08 11:14:12 +0300

    Add the appropriate header (AGPL).

--- CDP4Reporting/Views/ReportDesignerRibbon.xaml.cs
+++ CDP4Reporting/Views/ReportDesignerRibbon.xaml.cs
@@ -1,17 +1,36 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ReportingRibbonPage.xaml.cs" company="RHEA System S.A.">
-//   Copyright (c) 2020 RHEA System S.A.
+// <copyright file="ReportDesignerRibbon.xaml.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Merlin Bieze, Naron Phou, Patxi Ozkoidi, Alexander van Delft, Mihail Militaru
+//            Nathanael Smiechowski, Kamil Wojnowski
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System.ComponentModel.Composition;
+using CDP4Composition.Ribbon;
+using CDP4Reporting.ViewModels;
+using Microsoft.Practices.Prism.Mvvm;
+
 namespace CDP4Reporting.Views
 {
-    using System.ComponentModel.Composition;
-    using CDP4Composition.Navigation;
-    using CDP4Composition.Ribbon;
-    using CDP4Reporting.ViewModels;
-    using Microsoft.Practices.Prism.Mvvm;
-
     /// <summary>
     /// Interaction logic for ObjectBrowserRibbon
     /// </summary>
@@ -22,12 +41,6 @@ namespace CDP4Reporting.Views
         /// <summary>
         /// Initializes a new instance of the <see cref="ReportDesignerRibbon"/> class.
         /// </summary>
-        /// <param name="panelNavigationService">
-        /// The (MEF) injected <see cref="IPanelNavigationService"/> that is used to navigate to panels/browsers
-        /// </param>
-        /// <param name="dialogNavigationService">
-        /// The (MEF) injected <see cref="IDialogNavigationService"/> that is used to navigate to generic dialogs
-        /// </param>
         [ImportingConstructor]
         public ReportDesignerRibbon()
         {
