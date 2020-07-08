ef70f42417621baf961f322f1eaf93fab20158e1
Adrian Chivu <adrian.chivu@enea.com>
2020-07-08 11:02:47 +0300

    Add the appropriate header (AGPL) inside CDP4Reporting project.

--- CDP4Reporting/ViewModels/ReportDesignerViewModel.cs
+++ CDP4Reporting/ViewModels/ReportDesignerViewModel.cs
@@ -1,8 +1,28 @@
-﻿// -------------------------------------------------------------------------------------------------
-// <copyright file="RelationshipEditorViewModel.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015-2020 RHEA System S.A.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReportDesignerViewModel.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Merlin Bieze, Naron Phou, Patxi Ozkoidi, Alexander van Delft, Mihail Militaru
+//            Nathanael Smiechowski, Kamil Wojnowski
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
-// -------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CDP4Reporting.ViewModels
 {
@@ -42,6 +62,8 @@ namespace CDP4Reporting.ViewModels
         /// </param>
         /// <param name="thingDialogNavigationService"></param>
         /// <param name="panelNavigationService">The panel navigation service.</param>
+        /// <param name="dialogNavigationService">The dialog navigation service.</param>
+        /// <param name="pluginSettingsService">The plugin service.</param>
         public ReportDesignerViewModel(Iteration thing, Participant participant, ISession session, IThingDialogNavigationService thingDialogNavigationService, IPanelNavigationService panelNavigationService, IDialogNavigationService dialogNavigationService, IPluginSettingsService pluginSettingsService)
             : base(thing, session, thingDialogNavigationService, panelNavigationService, dialogNavigationService, pluginSettingsService)
         {
