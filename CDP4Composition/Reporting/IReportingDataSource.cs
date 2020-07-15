8650ba9be5d78c7fed825f5d314721cc3462bf59
Cozmin Velciu <cozmin.velciu@enea.com>
2020-07-15 17:47:48 +0300

    Rename interface to match naming rules

--- CDP4Composition/Reporting/IReportingDataSource.cs
+++ CDP4Composition/Reporting/IReportingDataSource.cs
@@ -27,7 +27,7 @@ namespace CDP4Composition.Reporting
 {
     using CDP4Common.EngineeringModelData;
 
-    public interface ICDP4ReportingDataSource<T> where T : ReportingDataSourceRowRepresentation, new()
+    public interface IReportingDataSource<T> where T : ReportingDataSourceRowRepresentation, new()
     {
         ReportingDataSourceClass<T> CreateDataSource(Iteration interation);
     }
