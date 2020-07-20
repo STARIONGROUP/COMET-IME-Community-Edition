b8c68be4fb938a84a1dc010c55a5a33e84bf5eeb
cozminvelciu <65681485+cozminvelciu@users.noreply.github.com>
2020-07-20 11:37:36 +0300

    Reporting tabular datasource framework (#492)
    * Initial framework implementation
    * Avoid forcing the user to declare a constructor
    * Simplify parameter identification on element
    * Move using declarations inside namespace
    * Add support for category filtering & row representations
    * Type parameters to row representation
    ** Needed for GetSibling/Children to properly work
    * Add visibility flag on row representation
    * Add unit tests
    * Add copyright header
    * Remove unused field
    * Rename interface to match naming rules
    * Remove reflection where not necessary
    * Remove code tasks
    * Replace First with SingleOrDefault
    * Specify AttributeUsage
    * Remove regions
    * Add XML docs

---
+++ CDP4Composition/Reporting/CategoryHierarchy.cs
@@ -0,0 +1,156 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CategoryHierarchy.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Cozmin Velciu, Adrian Chivu
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Composition.Reporting
+{
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Common.EngineeringModelData;
+
+    using System.Linq;
+
+    using CDP4Common.CommonData;
+
+    /// <summary>
+    /// Class representing a linear hierarchy of <see cref="CDP4Common.SiteDirectoryData.Category"/> items.
+    /// </summary>
+    public class CategoryHierarchy
+    {
+        /// <summary>
+        /// Builder class for a <see cref="CategoryHierarchy"/>.
+        /// </summary>
+        internal class Builder
+        {
+            /// <summary>
+            /// The <see cref="Iteration"/> containing the desired <see cref="CDP4Common.SiteDirectoryData.Category"/> items.
+            /// </summary>
+            private readonly Iteration iteration;
+
+            /// <summary>
+            /// The top element of the <see cref="CategoryHierarchy"/> to be constructed.
+            /// </summary>
+            private CategoryHierarchy top;
+
+            /// <summary>
+            /// The bottom element of the <see cref="CategoryHierarchy"/> to be constructed.
+            /// </summary>
+            private CategoryHierarchy current;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Builder"/> class.
+            /// </summary>
+            /// <param name="iteration">
+            /// The <see cref="Iteration"/> containing the desired <see cref="CDP4Common.SiteDirectoryData.Category"/> items.
+            /// </param>
+            /// <param name="topLevelCategoryShortName">
+            /// The <see cref="DefinedThing.ShortName"/> of the <see cref="CDP4Common.SiteDirectoryData.Category"/> of
+            /// the top element of the <see cref="CategoryHierarchy"/> to be constructed.
+            /// </param>
+            internal Builder(Iteration iteration, string topLevelCategoryShortName)
+            {
+                this.iteration = iteration;
+                this.AddLevel(topLevelCategoryShortName);
+            }
+
+            /// <summary>
+            /// Gets the <see cref="CDP4Common.SiteDirectoryData.Category"/> with the given <paramref name="shortName"/>
+            /// contained in the <see cref="iteration"/>.
+            /// </summary>
+            /// <param name="shortName">
+            /// The <see cref="DefinedThing.ShortName"/> of the desired <see cref="CDP4Common.SiteDirectoryData.Category"/>.
+            /// </param>
+            /// <returns>
+            /// The desired <see cref="CDP4Common.SiteDirectoryData.Category"/>.
+            /// </returns>
+            private Category GetCategoryByShortName(string shortName)
+            {
+                return this.iteration.Cache
+                    .Select(x => x.Value.Value)
+                    .OfType<Category>()
+                    .Single(x => x.ShortName == shortName);
+            }
+
+            /// <summary>
+            /// Adds a new level to the <see cref="CategoryHierarchy"/>.
+            /// </summary>
+            /// <param name="categoryShortName">
+            /// The <see cref="DefinedThing.ShortName"/> of the to-be-added <see cref="CDP4Common.SiteDirectoryData.Category"/>.
+            /// </param>
+            /// <returns>
+            /// This <see cref="Builder"/> object.
+            /// </returns>
+            public Builder AddLevel(string categoryShortName)
+            {
+                var category = this.GetCategoryByShortName(categoryShortName);
+
+                var newCategoryHierarchy = new CategoryHierarchy(category);
+
+                if (this.current == null)
+                {
+                    this.top = this.current = newCategoryHierarchy;
+                }
+                else
+                {
+                    this.current.Child = newCategoryHierarchy;
+                    this.current = this.current.Child;
+                }
+
+                return this;
+            }
+
+            /// <summary>
+            /// Finishes building the current <see cref="CategoryHierarchy"/>.
+            /// </summary>
+            /// <returns>
+            /// The build <see cref="CategoryHierarchy"/>.
+            /// </returns>
+            public CategoryHierarchy Build()
+            {
+                return this.top;
+            }
+        }
+
+        /// <summary>
+        /// The filtering <see cref="CDP4Common.SiteDirectoryData.Category"/> that must be matched on the associated level
+        /// of the <see cref="ReportingDataSourceRow{T}"/>.
+        /// </summary>
+        public readonly Category Category;
+
+        /// <summary>
+        /// The child node in this <see cref="CategoryHierarchy"/>'s linear hierarchy.
+        /// </summary>
+        public CategoryHierarchy Child { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryHierarchy"/> class.
+        /// </summary>
+        /// <param name="category">
+        /// This node's <see cref="Category"/>.
+        /// </param>
+        private CategoryHierarchy(Category category)
+        {
+            this.Category = category;
+        }
+    }
+}
---
+++ CDP4Composition/Reporting/ReportingDataSourceRowRepresentation.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReportingDataSourceRowRepresentation.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Cozmin Velciu, Adrian Chivu
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Composition.Reporting
+{
+    /// <summary>
+    /// Abstract base class from which all row representations for a <see cref="ReportingDataSourceClass{T}"/> need to derive.
+    /// </summary>
+    public abstract class ReportingDataSourceRowRepresentation
+    {
+        /// <summary>
+        /// The Element name, fully qualified with the path to the top element.
+        /// </summary>
+        internal string ElementName;
+
+        /// <summary>
+        /// Flag indicating whether the row matches the filtered criteria defined in <see cref="CategoryHierarchy"/>.
+        /// Note that when this is false, all values will be null on the row.
+        /// </summary>
+        internal bool IsVisible;
+    }
+}
