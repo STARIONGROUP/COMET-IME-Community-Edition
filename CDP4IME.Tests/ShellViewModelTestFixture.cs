4b20a31c41cfc4383db0a42bfd705055fedba1ec
nathanael smiechowski <n.smiechowski@rheagroup.com>
2020-07-30 23:15:33 +0200

    Fixed failing unit test, cosmetics changes in the concerned test fixture

--- CDP4IME.Tests/ShellViewModelTestFixture.cs
+++ CDP4IME.Tests/ShellViewModelTestFixture.cs
@@ -1,33 +1,60 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ShellViewModelTestFixture.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015 RHEA System S.A.
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Kamil Wojnowski
+//
+//    This file is part of CDP4-IME Community Edition. 
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace CDP4IME.Tests
 {
+    using Microsoft.Practices.ServiceLocation;
+
+    using Moq;
+
+    using NLog;
+    using NLog.Config;
+
+    using NUnit.Framework;
+
+    using ReactiveUI;
+
+    using System;
+    using System.Reactive.Concurrency;
     using System.Collections.Generic;
+    
     using CDP4Composition.Navigation;
+    
     using CDP4Dal;
     using CDP4Dal.Composition;
     using CDP4Dal.DAL;
+    using CDP4Dal.Events;
+
     using CDP4IME;
     using CDP4IME.ViewModels;
-    using CDP4ShellDialogs.ViewModels;
-    using Microsoft.Practices.ServiceLocation;
-    using Moq;
-    using NLog;
-    using NLog.Config;
-    using NUnit.Framework;
-    using System;
-    using CDP4Dal.Events;
-    using CDP4Composition.Navigation.Interfaces;
+    using CDP4IME.Settings;
 
-    using ReactiveUI;
+    using CDP4ShellDialogs.ViewModels;
+    
     using CDP4Common.SiteDirectoryData;
     using CDP4Common.EngineeringModelData;
-
-    using CDP4IME.Settings;
 
     /// <summary>
     /// TestFixture for the <see cref="ShellViewModel"/>
@@ -41,12 +68,7 @@ namespace CDP4IME.Tests
         /// the view-model under test
         /// </summary>
         private ShellViewModel viewModel;
-
-        /// <summary>
-        /// mocked <see cref="IDal"/>
-        /// </summary>
-        private Mock<IDal> mockedDal;
-
+        
         /// <summary>
         /// mocked <see cref="IDialogNavigationService"/>
         /// </summary>
@@ -79,6 +101,8 @@ namespace CDP4IME.Tests
         [SetUp]
         public void SetUp()
         {
+            RxApp.MainThreadScheduler = Scheduler.CurrentThread;
+            
             LogManager.Configuration = new LoggingConfiguration();
 
             this.navigationService = new Mock<IDialogNavigationService>();
@@ -88,10 +112,12 @@ namespace CDP4IME.Tests
             this.iteration = new Iteration(Guid.NewGuid(), null, this.uri) { IterationSetup = this.iterationSetup };
 
             this.session = new Mock<ISession>();
+            
             this.session.Setup(x=>x.OpenIterations).Returns(new Dictionary<Iteration, Tuple<DomainOfExpertise, Participant>>
             {
                 {this.iteration, new Tuple<DomainOfExpertise, Participant>(this.domain, null)}
             });
+
             this.serviceLocator = new Mock<IServiceLocator>();
             ServiceLocator.SetLocatorProvider(() => this.serviceLocator.Object);
 
