616316f76b7bedc59f468f6cd3e4a74e8623c981
kamilatrhea <k.wojnowski@rheagroup.com>
2020-09-08 14:33:30 +0200

    docs changed

--- RelationshipMatrix.Tests/ViewModel/SourceConfigurationViewModelTestFixture.cs
+++ RelationshipMatrix.Tests/ViewModel/SourceConfigurationViewModelTestFixture.cs
@@ -2,7 +2,7 @@
 // <copyright file="SourceConfigurationViewModelTestFixture.cs" company="RHEA System S.A.">
 //    Copyright (c) 2015-2020 RHEA System S.A.
 //
-//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Kamil W
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Kamil Wojnowski, Nathanael Smiechowski
 //
 //    This file is part of CDP4-IME Community Edition. 
 //    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
