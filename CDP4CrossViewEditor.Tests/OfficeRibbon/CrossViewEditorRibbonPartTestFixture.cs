06205a7cec8c414ef9e40892e726365443a0ea13
Adrian Chivu <adrian.chivu@enea.com>
2020-11-26 16:00:09 +0200

    Fix typo.

--- CDP4CrossViewEditor.Tests/OfficeRibbon/CrossViewEditorRibbonPartTestFixture.cs
+++ CDP4CrossViewEditor.Tests/OfficeRibbon/CrossViewEditorRibbonPartTestFixture.cs
@@ -276,7 +276,7 @@ namespace CDP4CrossViewEditor.Tests.OfficeRibbon
         }
 
         /// <summary>
-        /// Create new iteration in order to simulate model openning
+        /// Create a new iteration in order to simulate model openning
         /// </summary>
         /// <returns></returns>
         private Iteration CreateIteration()
