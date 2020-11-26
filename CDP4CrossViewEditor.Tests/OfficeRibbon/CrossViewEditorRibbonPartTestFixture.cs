08f43fd5a555078d77e38c5622906819247add4a
Adrian Chivu <adrian.chivu@enea.com>
2020-11-26 15:05:31 +0200

    Fixes: use spaces instead of tabs, cleanup, change file headers.

--- CDP4CrossViewEditor.Tests/OfficeRibbon/CrossViewEditorRibbonPartTestFixture.cs
+++ CDP4CrossViewEditor.Tests/OfficeRibbon/CrossViewEditorRibbonPartTestFixture.cs
@@ -2,7 +2,7 @@
 // <copyright file="CrossViewEditorRibbonPartTestFixture.cs" company="RHEA System S.A.">
 //    Copyright (c) 2015-2020 RHEA System S.A.
 //
-//    Author: Sam Gerené, Alex Vorobiev, Naron Phou, Patxi Ozkoidi, Alexander van Delft, Mihail Militaru.
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Cozmin Velciu, Adrian Chivu
 //
 //    This file is part of CDP4-IME Community Edition.
 //    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
@@ -276,7 +276,7 @@ namespace CDP4CrossViewEditor.Tests.OfficeRibbon
         }
 
         /// <summary>
-        /// Create new iteration in order to simulate model openning
+        /// Create a new iteration in order to simulate model opening
         /// </summary>
         /// <returns></returns>
         private Iteration CreateIteration()
