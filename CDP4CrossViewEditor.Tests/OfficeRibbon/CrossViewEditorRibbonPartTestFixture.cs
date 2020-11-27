50ee090cbd6f19bb262b7d998773b439d081bb16
Adrian Chivu <47980674+adrianchivu@users.noreply.github.com>
2020-11-27 12:15:27 +0200

    Crossview Editor create and document project structure (#622)
    * Setup a basic project structure and add ribbon button.
    
    * Remove unnecessary item from solution(at least for the moment).
    
    * Cosmetics.
    
    * Cosmetics.
    
    * Add test project for Crossview Editor.
    
    * Add basic tests.
    
    * Code fixes(dupplication).
    
    * Add test project to the unit test suite.
    
    * Fix wrong path to the dll.
    
    * Add tests.
    
    * Udpdate header files.
    
    * Spaces inside tabs.
    
    * Fix typo.
    
    * Fix typo.

---
+++ CDP4CrossViewEditor.Tests/OfficeRibbon/CrossViewEditorRibbonPartTestFixture.cs
@@ -0,0 +1,297 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CrossViewEditorRibbonPartTestFixture.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Cozmin Velciu, Adrian Chivu
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4CrossViewEditor.Tests.OfficeRibbon
+{
+    using System;
+    using System.Reactive.Concurrency;
+    using System.Threading;
+
+    using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Composition;
+    using CDP4Composition.Navigation;
+    using CDP4Composition.Navigation.Interfaces;
+    using CDP4Composition.PluginSettingService;
+
+    using CDP4Dal;
+    using CDP4Dal.Events;
+
+    using CDP4OfficeInfrastructure;
+
+    using Microsoft.Practices.ServiceLocation;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using ReactiveUI;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="CrossViewEditorRibbonPart"/>
+    /// </summary>
+    [TestFixture, Apartment(ApartmentState.STA)]
+    public class CrossViewEditorRibbonPartTestFixture
+    {
+        /// <summary>
+        /// The current ribbon button label
+        /// </summary>
+        private const string RibbonButtonId = "Editor";
+
+        /// <summary>
+        /// The current session associated <see cref="Assembler"></see>
+        /// </summary>
+        private Assembler assembler;
+
+        /// <summary>
+        /// The current assembler <see cref="Uri"/>
+        /// </summary>
+        private Uri uri;
+
+        /// <summary>
+        /// The <see cref="RibbonPart"/> under test
+        /// </summary>
+        private CrossViewEditorRibbonPart ribbonPart;
+
+        /// <summary>
+        /// Total amounf of Ribbon controls
+        /// </summary>
+        private int amountOfRibbonControls;
+
+        /// <summary>
+        /// Ribbon button index order
+        /// </summary>
+        private int order;
+
+        /// <summary>
+        /// Mock <see cref="IServiceLocator"/>
+        /// </summary>
+        private Mock<IServiceLocator> serviceLocator;
+
+        /// <summary>
+        /// Mock <see cref="IPanelNavigationService"/>
+        /// </summary>
+        private Mock<IPanelNavigationService> panelNavigationService;
+
+        /// <summary>
+        /// Mock <see cref="IThingDialogNavigationService"/>
+        /// </summary>
+        private Mock<IThingDialogNavigationService> thingDialogNavigationService;
+
+        /// <summary>
+        /// Mock <see cref="IDialogNavigationService"/>
+        /// </summary>
+        private Mock<IDialogNavigationService> dialogNavigationService;
+
+        /// <summary>
+        /// Mock <see cref="IPluginSettingsService"/>
+        /// </summary>
+        private Mock<IPluginSettingsService> pluginSettingsService;
+
+        /// <summary>
+        /// Mock <see cref="IOfficeApplicationWrapper"/>
+        /// </summary>
+        private Mock<IOfficeApplicationWrapper> officeApplicationWrapper;
+
+        /// <summary>
+        /// Mock <see cref="ISession"/>
+        /// </summary>
+        private Mock<ISession> session;
+
+        [SetUp]
+        public void SetUp()
+        {
+            RxApp.MainThreadScheduler = Scheduler.CurrentThread;
+
+            this.uri = new Uri("http://www.rheageoup.com");
+            this.assembler = new Assembler(this.uri);
+            this.session = new Mock<ISession>();
+            this.session.Setup(x => x.Assembler).Returns(this.assembler);
+
+            this.panelNavigationService = new Mock<IPanelNavigationService>();
+            this.thingDialogNavigationService = new Mock<IThingDialogNavigationService>();
+            this.dialogNavigationService = new Mock<IDialogNavigationService>();
+            this.serviceLocator = new Mock<IServiceLocator>();
+            this.officeApplicationWrapper = new Mock<IOfficeApplicationWrapper>();
+            this.pluginSettingsService = new Mock<IPluginSettingsService>();
+
+            this.amountOfRibbonControls = 2;
+            this.order = 1000;
+
+            this.ribbonPart = new CrossViewEditorRibbonPart(this.order, this.panelNavigationService.Object, this.thingDialogNavigationService.Object, this.dialogNavigationService.Object, this.pluginSettingsService.Object, this.officeApplicationWrapper.Object);
+
+            ServiceLocator.SetLocatorProvider(() => this.serviceLocator.Object);
+
+            this.serviceLocator.Setup(x => x.GetInstance<IThingDialogNavigationService>())
+                .Returns(this.thingDialogNavigationService.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CDPMessageBus.Current.ClearSubscriptions();
+        }
+
+        [Test]
+        public void VerifyThatTheOrderAndXmlAreLoaded()
+        {
+            Assert.AreEqual(this.order, this.ribbonPart.Order);
+            Assert.AreEqual(this.amountOfRibbonControls, this.ribbonPart.ControlIdentifiers.Count);
+        }
+
+        [Test]
+        public void VerifyThatIfFluentRibbonIsNotActiveTheSessionEventHasNoEffect()
+        {
+            var fluentRibbonManager = new FluentRibbonManager { IsActive = false };
+            fluentRibbonManager.RegisterRibbonPart(this.ribbonPart);
+
+            var sessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(sessionEvent);
+            Assert.IsNull(this.ribbonPart.Session);
+        }
+
+        [Test]
+        public void VerifyThatIfFluentRibbonIsNullTheSessionEventHasNoEffect()
+        {
+            var sessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(sessionEvent);
+            Assert.IsNull(this.ribbonPart.Session);
+        }
+
+        [Test]
+        public void VerifyThatRibbonPartHandlesSessionOpenAndCloseEvent()
+        {
+            var fluentRibbonManager = new FluentRibbonManager { IsActive = true };
+            fluentRibbonManager.RegisterRibbonPart(this.ribbonPart);
+
+            var openSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(openSessionEvent);
+
+            Assert.AreEqual(this.session.Object, this.ribbonPart.Session);
+
+            var closeSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Closed);
+            CDPMessageBus.Current.SendMessage(closeSessionEvent);
+
+            Assert.IsNull(this.ribbonPart.Session);
+        }
+
+        [Test]
+        public void VerifyThatGetEnabledReturnsExpectedResult()
+        {
+            var fluentRibbonManager = new FluentRibbonManager { IsActive = true };
+            fluentRibbonManager.RegisterRibbonPart(this.ribbonPart);
+
+            Assert.IsFalse(this.ribbonPart.GetEnabled(RibbonButtonId));
+
+            var openSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(openSessionEvent);
+
+            Assert.IsFalse(this.ribbonPart.GetEnabled(RibbonButtonId));
+
+            var closeSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Closed);
+            CDPMessageBus.Current.SendMessage(closeSessionEvent);
+
+            Assert.IsFalse(this.ribbonPart.GetEnabled(RibbonButtonId));
+        }
+
+        [Test]
+        public void VerifyThatButtonsAreEnabledWhenIterationIsLoaded()
+        {
+            var fluentRibbonManager = new FluentRibbonManager { IsActive = true };
+            fluentRibbonManager.RegisterRibbonPart(this.ribbonPart);
+
+            Assert.IsFalse(this.ribbonPart.GetEnabled(RibbonButtonId));
+
+            var openSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(openSessionEvent);
+            CDPMessageBus.Current.SendObjectChangeEvent(this.CreateIteration(), EventKind.Added);
+
+            Assert.IsTrue(this.ribbonPart.GetEnabled(RibbonButtonId));
+        }
+
+        [Test]
+        public void VerifyThatWhenSessionIsClosedAfterModelWasOpenIterationsAreCleaned()
+        {
+            var fluentRibbonManager = new FluentRibbonManager { IsActive = true };
+            fluentRibbonManager.RegisterRibbonPart(this.ribbonPart);
+
+            Assert.IsFalse(this.ribbonPart.GetEnabled(RibbonButtonId));
+
+            var openSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(openSessionEvent);
+            CDPMessageBus.Current.SendObjectChangeEvent(this.CreateIteration(), EventKind.Added);
+
+            Assert.IsTrue(this.ribbonPart.GetEnabled(RibbonButtonId));
+            Assert.AreEqual(1, this.ribbonPart.Iterations.Count);
+
+            var closeSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Closed);
+            CDPMessageBus.Current.SendMessage(closeSessionEvent);
+
+            Assert.IsFalse(this.ribbonPart.GetEnabled(RibbonButtonId));
+            Assert.AreEqual(0, this.ribbonPart.Iterations.Count);
+        }
+
+        [Test]
+        public void VerifyThatOnActionEditorWorks()
+        {
+            var fluentRibbonManager = new FluentRibbonManager { IsActive = true };
+            fluentRibbonManager.RegisterRibbonPart(this.ribbonPart);
+
+            var openSessionEvent = new SessionEvent(this.session.Object, SessionStatus.Open);
+            CDPMessageBus.Current.SendMessage(openSessionEvent);
+
+            var iteration = this.CreateIteration();
+
+            CDPMessageBus.Current.SendObjectChangeEvent(iteration, EventKind.Added);
+            Assert.AreEqual(1, this.ribbonPart.Iterations.Count);
+
+            this.ribbonPart.GetContent(RibbonButtonId);
+
+            Assert.DoesNotThrowAsync(async () => await this.ribbonPart.OnAction($"Editor_{iteration.Iid}", iteration.Iid.ToString()));
+
+            CDPMessageBus.Current.SendObjectChangeEvent(iteration, EventKind.Removed);
+            Assert.AreEqual(0, this.ribbonPart.Iterations.Count);
+        }
+
+        /// <summary>
+        /// Create a new iteration in order to simulate model opening
+        /// </summary>
+        /// <returns></returns>
+        private Iteration CreateIteration()
+        {
+            return new Iteration(Guid.NewGuid(), this.assembler.Cache, this.uri)
+            {
+                Container = new EngineeringModel(Guid.NewGuid(), this.assembler.Cache, this.uri)
+                {
+                    EngineeringModelSetup = new EngineeringModelSetup(Guid.NewGuid(), this.assembler.Cache, this.uri)
+                },
+                IterationSetup = new IterationSetup(Guid.NewGuid(), this.assembler.Cache, this.uri)
+                {
+                    IterationNumber = 1
+                }
+            };
+        }
+    }
+}
---
+++ CDP4CrossViewEditor/OfficeRibbon/CrossViewEditorRibbonPart.cs
@@ -0,0 +1,301 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CrossViewEditorRibbonPart.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Cozmin Velciu, Adrian Chivu
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4CrossViewEditor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reactive.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CDP4Common.EngineeringModelData;
+    using CDP4Composition;
+    using CDP4Composition.Navigation;
+    using CDP4Composition.Navigation.Interfaces;
+    using CDP4Composition.PluginSettingService;
+    using CDP4Dal;
+    using CDP4Dal.Events;
+    using CDP4OfficeInfrastructure;
+    using NLog;
+    using ReactiveUI;
+
+    /// <summary>
+    /// The purpose of the <see cref="CrossViewEditorRibbonPart"/> class is to describe and provide a part of the Fluent Ribbon
+    /// that is used in an Office addin. A <see cref="RibbonPart"/> always describes a ribbon group containing different controls
+    /// </summary>
+    public class CrossViewEditorRibbonPart : RibbonPart
+    {
+        /// <summary>
+        /// The NLog logger
+        /// </summary>
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// The <see cref="IOfficeApplicationWrapper"/> that provides access to the loaded Office application
+        /// </summary>
+        private readonly IOfficeApplicationWrapper officeApplicationWrapper;
+
+        /// <summary>
+        /// Gets the <see cref="ISession"/> that is active for the <see cref="RibbonPart"/>
+        /// </summary>
+        public ISession Session { get; private set; }
+
+        /// <summary>
+        /// Gets a List of <see cref="Iteration"/> that are opened
+        /// </summary>
+        public List<Iteration> Iterations { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CrossViewEditorRibbonPart"/> class.
+        /// </summary>
+        /// <param name="order">
+        /// The order in which the ribbon part is to be presented on the Office Ribbon
+        /// </param>
+        /// <param name="panelNavigationService">
+        /// The instance of <see cref="IPanelNavigationService"/> that orchestrates navigation of <see cref="IPanelView"/>
+        /// </param>
+        /// <param name="thingDialogNavigationService">
+        /// The instance of <see cref="IThingDialogNavigationService"/> that orchestrates navigation of <see cref="IThingDialogView"/>
+        /// </param>
+        /// <param name="dialogNavigationService">
+        /// The instance of <see cref="IDialogNavigationService"/> that orchestrates navigation to dialogs
+        /// </param>
+        /// <param name="pluginSettingsService">
+        /// The <see cref="IPluginSettingsService"/> used to read and write plugin setting files.
+        /// </param>
+        /// <param name="officeApplicationWrapper">
+        /// The instance of <see cref="IOfficeApplicationWrapper"/> that provides access to the loaded Office application.
+        /// </param>
+        public CrossViewEditorRibbonPart(int order, IPanelNavigationService panelNavigationService, IThingDialogNavigationService thingDialogNavigationService, IDialogNavigationService dialogNavigationService, IPluginSettingsService pluginSettingsService, IOfficeApplicationWrapper officeApplicationWrapper)
+            : base(order, panelNavigationService, thingDialogNavigationService, dialogNavigationService, pluginSettingsService)
+        {
+            this.officeApplicationWrapper = officeApplicationWrapper;
+            this.Iterations = new List<Iteration>();
+
+            CDPMessageBus.Current.Listen<SessionEvent>().Subscribe(this.SessionChangeEventHandler);
+
+            CDPMessageBus.Current.Listen<ObjectChangedEvent>(typeof(Iteration))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(this.IterationChangeEventHandler);
+        }
+
+        /// <summary>
+        /// Gets the the current executing assembly
+        /// </summary>
+        /// <returns>
+        /// an instance of <see cref="Assembly"/>
+        /// </returns>
+        protected override Assembly GetCurrentAssembly()
+        {
+            return Assembly.GetExecutingAssembly();
+        }
+
+        /// <summary>
+        /// Gets the name of the resource that contains the Ribbon XML
+        /// </summary>
+        /// <returns>
+        /// The name of the ribbon XML resource
+        /// </returns>
+        protected override string GetRibbonXmlResourceName()
+        {
+            return "CrossViewEditorRibbon";
+        }
+
+        /// <summary>
+        /// The event-handler that is invoked by the subscription that listens for updates
+        /// on the <see cref="Session"/> that is being represented by the view-model
+        /// </summary>
+        /// <param name="sessionChange">
+        /// The payload of the event that is being handled
+        /// </param>
+        private void SessionChangeEventHandler(SessionEvent sessionChange)
+        {
+            if (this.FluentRibbonManager == null)
+            {
+                return;
+            }
+
+            if (!this.FluentRibbonManager.IsActive)
+            {
+                return;
+            }
+
+            switch (sessionChange.Status)
+            {
+                case SessionStatus.Open:
+                    this.Session = sessionChange.Session;
+                    return;
+
+                case SessionStatus.Closed:
+                    this.Iterations.Clear();
+                    this.Session = null;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="ObjectChangedEvent"/> handler for <see cref="Iteration"/>
+        /// </summary>
+        /// <param name="iterationEvent">The <see cref="ObjectChangedEvent"/></param>
+        private void IterationChangeEventHandler(ObjectChangedEvent iterationEvent)
+        {
+            if (this.Session == null)
+            {
+                return;
+            }
+
+            switch (iterationEvent.EventKind)
+            {
+                case EventKind.Added:
+                    this.Iterations.Add(iterationEvent.ChangedThing as Iteration);
+                    break;
+
+                case EventKind.Removed:
+                {
+                    var iteration = iterationEvent.ChangedThing as Iteration;
+                    this.Iterations.RemoveAll(x => x == iteration);
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Invokes the action as a result of a ribbon control being clicked, selected, etc.
+        /// </summary>
+        /// <param name="ribbonControlId">
+        /// The Id property of the associated RibbonControl
+        /// </param>
+        /// <param name="ribbonControlTag">
+        /// The Tag property of the associated RibbonControl
+        /// </param>
+        public override async Task OnAction(string ribbonControlId, string ribbonControlTag = "")
+        {
+            if (this.Session == null)
+            {
+                logger.Error("The current session is null");
+                return;
+            }
+
+            if (ribbonControlId.StartsWith("Editor_"))
+            {
+                await Task.Delay(1000);
+            }
+            else
+            {
+                logger.Debug($"The ribbon control with Id {ribbonControlId} and Tag {ribbonControlTag} is not handled by the current RibbonPart");
+            }
+        }
+
+        /// <summary>
+        /// Gets the the content of a Dynamic Menu
+        /// </summary>
+        /// <param name="ribbonControlId">
+        /// The Id property of the associated RibbonControl
+        /// </param>
+        /// <param name="ribbonControlTag">
+        /// The Tag property of the associated RibbonControl
+        /// </param>
+        /// <returns>
+        /// Ribbon XML that is the content of the Dynamic Menu
+        /// </returns>
+        public override string GetContent(string ribbonControlId, string ribbonControlTag = "")
+        {
+            var menuxml = string.Empty;
+
+            if (ribbonControlId != "Editor")
+            {
+                return menuxml;
+            }
+
+            var sb = new StringBuilder(@"<menu xmlns=""http://schemas.microsoft.com/office/2006/01/customui"">");
+
+            foreach (var iteration in this.Iterations)
+            {
+                var engineeringModel = (EngineeringModel) iteration.Container;
+
+                var selectedDomainOfExpertise = this.Session.QuerySelectedDomainOfExpertise(iteration);
+
+                var domainShortName = selectedDomainOfExpertise == null ? string.Empty : selectedDomainOfExpertise.ShortName;
+                var label = $"{engineeringModel.EngineeringModelSetup.ShortName} - {iteration.IterationSetup.IterationNumber} : [{domainShortName}]";
+
+                var menuContent = $"<button id=\"Editor_{iteration.Iid}\" label=\"{label}\" onAction=\"OnAction\" tag=\"{0}\" />";
+                sb.Append(menuContent);
+            }
+
+            sb.Append(@"</menu>");
+            menuxml = sb.ToString();
+
+            this.UpdateControlIdentifiers(menuxml);
+
+            return menuxml;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether a control is enabled or disabled
+        /// </summary>
+        /// <param name="ribbonControlId">
+        /// The Id property of the associated RibbonControl
+        /// </param>
+        /// <param name="ribbonControlTag">
+        /// The Tag property of the associated RibbonControl
+        /// </param>
+        /// <returns>
+        /// true if enabled, false if not enabled
+        /// </returns>
+        public override bool GetEnabled(string ribbonControlId, string ribbonControlTag = "")
+        {
+            return ribbonControlId == "Editor" && this.IsEditorEnabled();
+        }
+
+        /// <summary>
+        /// Gets the label as a <see cref="string"/> for the control
+        /// </summary>
+        /// <param name="ribbonControlId">
+        /// The Id property of the associated RibbonControl
+        /// </param>
+        /// <param name="ribbonControlTag">
+        /// The Tag property of the associated RibbonControl
+        /// </param>
+        /// <returns>
+        /// A string that represents the content of the label(min length of 1 character, max length of 1024 chars)
+        /// </returns>
+        public override string GetLabel(string ribbonControlId, string ribbonControlTag = "")
+        {
+            return ribbonControlId == "Editor" ? "Editor" : string.Empty;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the Editor button on the Ribbon is active or not
+        /// </summary>
+        /// <returns>
+        /// returns true if the Rebuild is enabled, false otherwise
+        /// </returns>
+        private bool IsEditorEnabled()
+        {
+            return this.Iterations.Count != 0 && this.Session != null;
+        }
+    }
+}
