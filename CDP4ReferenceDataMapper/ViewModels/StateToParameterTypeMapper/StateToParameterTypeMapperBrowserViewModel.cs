fe6cfd4677671d077ddb9573a3072adda6aaf21a
samatrhea <s.gerene@rheagroup.com>
2020-12-05 16:27:48 +0100

    [Add] CDP4ReferenceDataMapper plugin project [Add] StateToParameterTypeMapperBrowserViewModel and StateToParameterTypeMapperBrowser [Add] StateToParameterTypeMapperRibbon [Add] ReferenceDataMapperModule

---
+++ CDP4ReferenceDataMapper/ViewModels/StateToParameterTypeMapper/StateToParameterTypeMapperBrowserViewModel.cs
@@ -0,0 +1,85 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StateToParameterTypeMapperBrowserViewModel.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2021 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Naron Phou, Alexander van Delft, Nathanael Smiechowski, Ahmed Abulwafa Ahmed
+//
+//    This file is part of CDP4-IME Community Edition. 
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4ReferenceDataMapper.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using CDP4Common.EngineeringModelData;
+
+    using CDP4Composition;
+    using CDP4Composition.DragDrop;
+    using CDP4Composition.Mvvm;
+    using CDP4Composition.Navigation;
+    using CDP4Composition.Navigation.Interfaces;
+    using CDP4Composition.PluginSettingService;
+
+    using CDP4Dal;
+
+    public class StateToParameterTypeMapperBrowserViewModel : BrowserViewModelBase<Iteration>, IPanelViewModel
+    {
+        /// <summary>
+        /// Backing field for <see cref="CurrentModel"/>
+        /// </summary>
+        private string currentModel;
+
+        /// <summary>
+        /// Backing field for <see cref="CurrentIteration"/>
+        /// </summary>
+        private int currentIteration;
+
+        /// <summary>
+        /// The Panel Caption
+        /// </summary>
+        private const string PanelCaption = "Actual Finite State to ParameterType mapping";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateToParameterTypeMapperBrowserViewModel"/> class
+        /// </summary>
+        /// <param name="iteration">The <see cref="EngineeringModel"/></param>
+        /// <param name="session">The session</param>
+        /// <param name="thingDialogNavigationService">the <see cref="IThingDialogNavigationService"/></param>
+        /// <param name="panelNavigationService">the <see cref="IPanelNavigationService"/></param>
+        /// <param name="dialogNavigationService">The <see cref="IDialogNavigationService"/></param>
+        /// <param name="pluginSettingsService">
+        /// The <see cref="IPluginSettingsService"/> used to read and write plugin setting files.
+        /// </param>
+        public StateToParameterTypeMapperBrowserViewModel(Iteration iteration, ISession session, IThingDialogNavigationService thingDialogNavigationService, IPanelNavigationService panelNavigationService, IDialogNavigationService dialogNavigationService, IPluginSettingsService pluginSettingsService)
+            : base(iteration, session, thingDialogNavigationService, panelNavigationService, dialogNavigationService, pluginSettingsService)
+        {
+            this.Caption = $"{PanelCaption}, iteration_{this.Thing.IterationSetup.IterationNumber}";
+            this.ToolTip = $"{((EngineeringModel)this.Thing.Container).EngineeringModelSetup.Name}\n{this.Thing.IDalUri}\n{this.Session.ActivePerson.Name}";
+
+            //this.UpdateOptions();
+
+            //this.AddSubscriptions();
+            //this.UpdateProperties();
+        }
+    }
+}
