1f7b8c00e8c017fa0ac572c8afbc452e459f9f32
Adrian Chivu <adrian.chivu@enea.com>
2021-01-28 00:28:23 +0200

    Replace observable with command.

--- CDP4CrossViewEditor/ViewModels/ParameterTypeSelectorViewModel.cs
+++ CDP4CrossViewEditor/ViewModels/ParameterTypeSelectorViewModel.cs
@@ -83,6 +83,11 @@ namespace CDP4CrossViewEditor.ViewModels
         public ReactiveList<ParameterTypeRowViewModel> SelectedTargetList { get; set; }
 
         /// <summary>
+        /// Gets/sets the move power parameter types command <see cref="ReactiveCommand"/>
+        /// </summary>
+        public ReactiveCommand<object> PowerParametersCommand { get; private set; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="ParameterTypeSelectorViewModel"/> class.
         /// </summary>
         /// <param name="iteration">Current opened iteration <see cref="Iteration"/></param>
@@ -111,17 +116,23 @@ namespace CDP4CrossViewEditor.ViewModels
                 ChangeTrackingEnabled = true
             };
 
-            this.WhenAnyValue(vm => vm.PowerParametersEnabled).Subscribe(isPowerClicked =>
+            this.PowerParametersCommand = ReactiveCommand.Create();
+            this.PowerParametersCommand.Subscribe(_ => this.ExecutePowerParametersCommand());
+        }
+
+        /// <summary>
+        /// Executes the <see cref="PowerParametersCommand"/>
+        /// </summary>
+        private void ExecutePowerParametersCommand()
+        {
+            if (this.PowerParametersEnabled)
             {
-                if (isPowerClicked)
-                {
-                    this.ExecuteMovePowerToTarget();
-                }
-                else
-                {
-                    this.ExecuteMovePowerToSource();
-                }
-            });
+                this.ExecuteMovePowerToTarget();
+            }
+            else
+            {
+                this.ExecuteMovePowerToSource();
+            }
         }
 
         /// <summary>
@@ -194,6 +205,9 @@ namespace CDP4CrossViewEditor.ViewModels
         protected internal override void ExecuteMoveToSource()
         {
             ExecuteMove(this.ParameterTypeTargetList, this.ParameterTypeSourceList, this.SelectedTargetList);
+
+            this.PowerParametersEnabled = PowerParameters
+                                              .Select(shortName => this.ParameterTypeTargetList.FirstOrDefault(row => string.Equals(row.Thing.ShortName, shortName, StringComparison.InvariantCultureIgnoreCase))).Count(row => row != null) == PowerParameters.Length;
         }
 
         /// <summary>
@@ -202,6 +216,9 @@ namespace CDP4CrossViewEditor.ViewModels
         protected internal override void ExecuteMoveToTarget()
         {
             ExecuteMove(this.ParameterTypeSourceList, this.ParameterTypeTargetList, this.SelectedSourceList);
+
+            this.PowerParametersEnabled = PowerParameters
+                                              .Select(shortName => this.ParameterTypeTargetList.FirstOrDefault(row => string.Equals(row.Thing.ShortName, shortName, StringComparison.InvariantCultureIgnoreCase))).Count(row => row != null) == PowerParameters.Length;
         }
 
         /// <summary>
@@ -235,7 +252,7 @@ namespace CDP4CrossViewEditor.ViewModels
         private void ExecuteMovePowerToSource()
         {
             var powerParameterTypes = PowerParameters
-                .Select(shortName => this.ParameterTypeSourceList.FirstOrDefault(row => string.Equals(row.Thing.ShortName, shortName, StringComparison.InvariantCultureIgnoreCase)))
+                .Select(shortName => this.ParameterTypeTargetList.FirstOrDefault(row => string.Equals(row.Thing.ShortName, shortName, StringComparison.InvariantCultureIgnoreCase)))
                 .Where(row => row != null);
 
             this.SelectedTargetList.Clear();
