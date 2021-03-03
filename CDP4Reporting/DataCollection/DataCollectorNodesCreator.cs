c8107576897078f28d82b215601e4aa27ca4946b
lxatrhea <a.vandelft@rheagroup.com>
2021-03-03 09:24:23 +0100

    Cosmetic fix

--- CDP4Reporting/DataCollection/DataCollectorNodesCreator.cs
+++ CDP4Reporting/DataCollection/DataCollectorNodesCreator.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="NestedElementTreeDataCollector.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2020 RHEA System S.A.
+// <copyright file="DataCollectorNodesCreator.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2021 RHEA System S.A.
 //
 //    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Cozmin Velciu, Adrian Chivu
 //
