c54d88315235f62ac3afe87ffa49b1579c5bafa8
samatrhea <s.gerene@rheagroup.com>
2021-03-03 22:23:53 +0100

    [Add] logging orchestration to EngineeringModel and Requirements plugin ribbonviewmodels

--- Requirements/ViewModels/RequirementBrowser/RequirementRibbonViewModel.cs
+++ Requirements/ViewModels/RequirementBrowser/RequirementRibbonViewModel.cs
@@ -1,25 +1,56 @@
-﻿// -------------------------------------------------------------------------------------------------
-// <copyright file="RequirementViewRibbonViewModel.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015 RHEA System S.A.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RequirementRibbonViewModel.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2021 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Ahmed Abulwafa Ahmed
+//
+//    This file is part of CDP4-IME Community Edition. 
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
-// -------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CDP4Requirements.ViewModels
 {
     using System;
+    using System.Diagnostics;
+
     using CDP4Common.EngineeringModelData;
+
     using CDP4Composition.Mvvm;
     using CDP4Composition.Navigation;
     using CDP4Composition.Navigation.Interfaces;
     using CDP4Composition.PluginSettingService;
+
     using CDP4Dal;
+
     using CDP4Requirements.Views;
+
+    using NLog;
 
     /// <summary>
     /// The view model for the ribbon control <see cref="RequirementBrowserRibbon"/>
     /// </summary>
     public class RequirementRibbonViewModel : RibbonButtonIterationDependentViewModel
     {
+        /// <summary>
+        /// The logger for the current class
+        /// </summary>
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RequirementRibbonViewModel"/> class
         /// </summary>
@@ -39,13 +70,18 @@ namespace CDP4Requirements.ViewModels
         /// <returns>An instance of <see cref="RequirementsBrowserViewModel"/></returns>
         public static RequirementsBrowserViewModel InstantiatePanelViewModel(Iteration iteration, ISession session, IThingDialogNavigationService thingDialogNavigationService, IPanelNavigationService panelNavigationService, IDialogNavigationService dialogNavigationService, IPluginSettingsService pluginSettingsService)
         {
+            var stopWatch = Stopwatch.StartNew();
+
             var model = iteration.Container as EngineeringModel;
             if (model == null)
             {
                 throw new InvalidOperationException("The container of an Iteration cannot be anything else than an Engineering Model.");
             }
 
-            return new RequirementsBrowserViewModel(iteration, session, thingDialogNavigationService, panelNavigationService, dialogNavigationService, pluginSettingsService);
+            var viewModel = new RequirementsBrowserViewModel(iteration, session, thingDialogNavigationService, panelNavigationService, dialogNavigationService, pluginSettingsService);
+            stopWatch.Stop();
+            Logger.Info("Open RequirementsBrowserViewModel took {0}", stopWatch.Elapsed);
+            return viewModel;
         }
     }
 }
