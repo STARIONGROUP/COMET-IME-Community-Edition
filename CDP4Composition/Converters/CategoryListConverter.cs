93ae8427459a2f403b9cb993686edc7c3ca0733b
SimonWoodRhea <86828242+SimonWoodRhea@users.noreply.github.com>
2021-09-29 11:46:21 +0200

    Add Category column to product tree and element definition view fixes… (#940)

---
+++ CDP4Composition/Converters/CategoryListConverter.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CategoryListConverter.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2021 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Simon Wood
+//
+//    This file is part of CDP4-IME Community Edition.
+//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Composition.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows.Data;
+
+    using CDP4Common.SiteDirectoryData;
+
+    /// <summary>
+    /// Converter that takes an <see cref="IEnumerable{Category}"/> and converts it to a comma seperated list.
+    /// </summary>
+    public class CategoryListConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a <see cref="IEnumerable{Category}"/> to a <see cref="string"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="IEnumerable{Category}"/>to convert from</param>
+        /// <param name="targetType">The target type.</param>
+        /// <param name="parameter">The converter parameter.</param>
+        /// <param name="culture">The supplied culture</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not IEnumerable<Category> categories)
+            {
+                throw new ArgumentException($"Parameter {nameof(value)} is not of type {nameof(IEnumerable<Category>)}", nameof(value));
+            }
+
+            return string.Join(", ", categories.Select(c => c.Name));
+        }
+
+        /// <summary>
+        /// Does nothing.
+        /// </summary>
+        /// <param name="value">
+        /// The incoming collection.
+        /// </param>
+        /// <param name="targetType">
+        /// The target type.
+        /// </param>
+        /// <param name="parameter">
+        /// The parameter passed on to this conversion.
+        /// </param>
+        /// <param name="culture">
+        /// The culture information.
+        /// </param>
+        /// <returns>
+        /// Throws <see cref="NotImplementedException"/> always.
+        /// </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
