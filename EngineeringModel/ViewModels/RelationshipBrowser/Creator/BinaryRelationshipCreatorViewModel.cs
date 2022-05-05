5982cb6acc17b7384549a05e9c5a002386e4c538
Omar Elebiary <o.elebiary@rheagroup.com>
2022-05-05 14:34:30 +0200

    Fix BinaryRelationship creation using quick screen; fixes #983

--- EngineeringModel/ViewModels/RelationshipBrowser/Creator/BinaryRelationshipCreatorViewModel.cs
+++ EngineeringModel/ViewModels/RelationshipBrowser/Creator/BinaryRelationshipCreatorViewModel.cs
@@ -1,23 +1,23 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="BinaryRelationshipCreatorViewModel.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2021 RHEA System S.A.
-//
-//    Author: Sam Gerené, Alex Vorobiev, Merlin Bieze, Naron Phou, Alexander van Delft, Nathanael Smiechowski
-//
-//    This file is part of CDP4-IME Community Edition. 
-//    The CDP4-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    Copyright (c) 2015-2022 RHEA System S.A.
+// 
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Antoine Théate, Omar Elebiary
+// 
+//    This file is part of COMET-IME Community Edition.
+//    The COMET-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
 //    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
-//
-//    The CDP4-IME Community Edition is free software; you can redistribute it and/or
+// 
+//    The COMET-IME Community Edition is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU Affero General Public
 //    License as published by the Free Software Foundation; either
 //    version 3 of the License, or any later version.
-//
-//    The CDP4-IME Community Edition is distributed in the hope that it will be useful,
+// 
+//    The COMET-IME Community Edition is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //    GNU Affero General Public License for more details.
-//
+// 
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
@@ -34,16 +34,16 @@ namespace CDP4EngineeringModel.ViewModels
     using CDP4Common.CommonData;
     using CDP4Common.EngineeringModelData;
     using CDP4Common.SiteDirectoryData;
-    
+
     using CDP4Dal;
     using CDP4Dal.Events;
-    
+
     using NLog;
-    
+
     using ReactiveUI;
 
     /// <summary>
-    /// The view-model that gives the capability to create a <see cref="BinaryRelationship"/> through means of drag and drop
+    /// The view-model that gives the capability to create a <see cref="BinaryRelationship" /> through means of drag and drop
     /// </summary>
     public class BinaryRelationshipCreatorViewModel : ReactiveObject, IRelationshipCreatorViewModel
     {
@@ -53,19 +53,9 @@ namespace CDP4EngineeringModel.ViewModels
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         /// <summary>
-        /// Backing field for <see cref="CanCreate"/>
-        /// </summary>
-        private bool canCreate;
-
-        /// <summary>
-        /// The <see cref="Iteration"/>
+        /// The <see cref="Iteration" />
         /// </summary>
         private readonly Iteration iteration;
-
-        /// <summary>
-        /// Backing field for <see cref="AppliedCategories"/>
-        /// </summary>
-        private List<Category> appliedCategories;
 
         /// <summary>
         /// The collection of subscriptions to dispose of
@@ -73,20 +63,30 @@ namespace CDP4EngineeringModel.ViewModels
         private readonly List<IDisposable> Subscriptions = new List<IDisposable>();
 
         /// <summary>
-        /// The current <see cref="ModelReferenceDataLibrary"/>
+        /// Backing field for <see cref="AppliedCategories" />
         /// </summary>
-        private ModelReferenceDataLibrary requiredRdl;
+        private List<Category> appliedCategories;
 
         /// <summary>
-        /// Backing field for <see cref="Name"/>
+        /// Backing field for <see cref="CanCreate" />
+        /// </summary>
+        private bool canCreate;
+
+        /// <summary>
+        /// Backing field for <see cref="Name" />
         /// </summary>
         private string name;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BinaryRelationshipCreatorViewModel"/> class
+        /// The current <see cref="ModelReferenceDataLibrary" />
         /// </summary>
-        /// <param name="iteration">The current <see cref="Iteration"/></param>
-        /// <param name="session">The <see cref="ISession"/> that was used to retrieve data from the datastore</param>
+        private ModelReferenceDataLibrary requiredRdl;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryRelationshipCreatorViewModel" /> class
+        /// </summary>
+        /// <param name="iteration">The current <see cref="Iteration" /></param>
+        /// <param name="session">The <see cref="ISession" /> that was used to retrieve data from the datastore</param>
         public BinaryRelationshipCreatorViewModel(Iteration iteration, ISession session)
         {
             this.Session = session;
@@ -94,8 +94,7 @@ namespace CDP4EngineeringModel.ViewModels
             this.PossibleCategories = new ReactiveList<Category>();
             this.SourceViewModel = new RelatedThingViewModel();
             this.TargetViewModel = new RelatedThingViewModel();
-            var relatedThingChangedSubscriber = this.WhenAnyValue(x => x.TargetViewModel.RelatedThing).
-                Subscribe(x => this.CanCreate = this.SourceViewModel.RelatedThing != null && this.TargetViewModel.RelatedThing != null);
+            var relatedThingChangedSubscriber = this.WhenAnyValue(x => x.TargetViewModel.RelatedThing, y => y.SourceViewModel.RelatedThing).Subscribe(x => this.CanCreate = (this.SourceViewModel.RelatedThing != null) && (this.TargetViewModel.RelatedThing != null));
             this.Subscriptions.Add(relatedThingChangedSubscriber);
 
             this.InitializeRequiredRdlSubscription();
@@ -104,60 +103,57 @@ namespace CDP4EngineeringModel.ViewModels
         }
 
         /// <summary>
-        /// The view-model for the user control to select the source of a <see cref="BinaryRelationship"/> to create
+        /// The view-model for the user control to select the source of a <see cref="BinaryRelationship" /> to create
         /// </summary>
         public RelatedThingViewModel SourceViewModel { get; private set; }
 
         /// <summary>
-        /// The view-model for the user control to select the target of a <see cref="BinaryRelationship"/> to create
+        /// The view-model for the user control to select the target of a <see cref="BinaryRelationship" /> to create
         /// </summary>
         public RelatedThingViewModel TargetViewModel { get; private set; }
 
         /// <summary>
-        /// Gets the possible <see cref="Category"/> for the <see cref="BinaryRelationship"/> to create
+        /// Gets the possible <see cref="Category" /> for the <see cref="BinaryRelationship" /> to create
         /// </summary>
         public ReactiveList<Category> PossibleCategories { get; private set; }
 
         /// <summary>
-        /// Gets the <see cref="ISession"/>
+        /// Gets the <see cref="ISession" />
         /// </summary>
         public ISession Session { get; }
 
         /// <summary>
-        /// Gets or sets the Name for the <see cref="BinaryRelationship"/> to create
+        /// Gets or sets the Name for the <see cref="BinaryRelationship" /> to create
         /// </summary>
         [CDPVersion("1.2.0")]
         public string Name
         {
-            get { return this.name; }
-            set { this.RaiseAndSetIfChanged(ref this.name, value); }
+            get => this.name;
+            set => this.RaiseAndSetIfChanged(ref this.name, value);
         }
 
         /// <summary>
-        /// Gets or sets the <see cref="Category"/>s for the <see cref="BinaryRelationship"/> to create
+        /// Gets or sets the <see cref="Category" />s for the <see cref="BinaryRelationship" /> to create
         /// </summary>
         public List<Category> AppliedCategories
         {
-            get { return this.appliedCategories; }
-            set { this.RaiseAndSetIfChanged(ref this.appliedCategories, value); }
+            get => this.appliedCategories;
+            set => this.RaiseAndSetIfChanged(ref this.appliedCategories, value);
         }
 
         /// <summary>
-        /// Gets a value indicating whether a <see cref="BinaryRelationship"/> can be created
+        /// Gets a value indicating whether a <see cref="BinaryRelationship" /> can be created
         /// </summary>
         public bool CanCreate
         {
-            get { return this.canCreate; }
-            private set { this.RaiseAndSetIfChanged(ref this.canCreate, value); }
+            get => this.canCreate;
+            private set => this.RaiseAndSetIfChanged(ref this.canCreate, value);
         }
 
         /// <summary>
-        /// Gets the type of the <see cref="Relationship"/> to create
+        /// Gets the type of the <see cref="Relationship" /> to create
         /// </summary>
-        public string CreatorKind
-        {
-            get { return "Binary Relationship"; }
-        }
+        public string CreatorKind => "Binary Relationship";
 
         /// <summary>
         /// Re-initializes the view-model
@@ -171,9 +167,9 @@ namespace CDP4EngineeringModel.ViewModels
         }
 
         /// <summary>
-        /// Creates a <see cref="Relationship"/> 
+        /// Creates a <see cref="Relationship" />
         /// </summary>
-        /// <returns>A new instance of <see cref="Relationship"/></returns>
+        /// <returns>A new instance of <see cref="Relationship" /></returns>
         public Relationship CreateRelationshipObject()
         {
             var relationship = new BinaryRelationship
@@ -182,6 +178,7 @@ namespace CDP4EngineeringModel.ViewModels
                 Source = this.SourceViewModel.RelatedThing,
                 Target = this.TargetViewModel.RelatedThing
             };
+
             relationship.Category.AddRange(this.AppliedCategories);
 
             return relationship;
@@ -207,7 +204,7 @@ namespace CDP4EngineeringModel.ViewModels
         }
 
         /// <summary>
-        /// Initializes the <see cref="requiredRdl"/> field
+        /// Initializes the <see cref="requiredRdl" /> field
         /// </summary>
         private void InitializeRequiredRdlSubscription()
         {
@@ -215,19 +212,21 @@ namespace CDP4EngineeringModel.ViewModels
             this.requiredRdl = modelsetup.RequiredRdl.Single();
 
             var rdl = (ReferenceDataLibrary)this.requiredRdl;
+
             while (rdl != null)
             {
                 var subscriber = CDPMessageBus.Current.Listen<ObjectChangedEvent>(rdl)
                     .Where(msg => msg.EventKind == EventKind.Updated)
                     .ObserveOn(RxApp.MainThreadScheduler)
                     .Subscribe(_ => this.PopulateCategories());
+
                 this.Subscriptions.Add(subscriber);
                 rdl = rdl.RequiredRdl;
             }
         }
 
         /// <summary>
-        /// Populates the possible <see cref="Category"/>
+        /// Populates the possible <see cref="Category" />
         /// </summary>
         private void PopulateCategories()
         {
