87364dbb7784cbd65568a1eb15421a611676fdce
Omar Elebiary <104059834+omaratrhea@users.noreply.github.com>
2022-05-05 16:04:32 +0200

    Fix edit permissions in Requirement Specification Editor; fixes #838 (#1053)
    * Fix edit permissions in Requirement Specification Editor; fixes #838
    
    * Add comment
    
    * Update unit tests
    
    * Cleanup code and update file headers
    
    * Update test fixture

--- Requirements/ViewModels/RequirementsSpecificationEditor/RequirementRowViewModel.cs
+++ Requirements/ViewModels/RequirementsSpecificationEditor/RequirementRowViewModel.cs
@@ -1,8 +1,27 @@
-﻿// ------------------------------------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="RequirementRowViewModel.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015-2019 RHEA System S.A.
+//    Copyright (c) 2015-2022 RHEA System S.A.
+// 
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Antoine Théate, Omar Elebiary
+// 
+//    This file is part of COMET-IME Community Edition.
+//    The COMET-IME Community Edition is the RHEA Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//    The COMET-IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+// 
+//    The COMET-IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
-// ------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
 {
@@ -11,79 +30,93 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
     using System.Linq;
     using System.Reactive;
     using System.Threading.Tasks;
+
     using CDP4Common.CommonData;
     using CDP4Common.EngineeringModelData;
+
     using CDP4CommonView.EventAggregator;
     using CDP4CommonView.ViewModels;
+
     using CDP4Composition.Mvvm;
+
     using CDP4Dal;
     using CDP4Dal.Events;
+
     using CDP4Requirements.Utils;
+
     using ReactiveUI;
 
     /// <summary>
-    /// The <see cref="RequirementRowViewModel"/> is the view-model that represents a <see cref="Requirement"/> in the <see cref="RequirementsSpecificationEditorViewModel"/>
+    /// The <see cref="RequirementRowViewModel" /> is the view-model that represents a <see cref="Requirement" /> in the
+    /// <see cref="RequirementsSpecificationEditorViewModel" />
     /// </summary>
     public class RequirementRowViewModel : CDP4CommonView.RequirementRowViewModel, IBreadCrumb
     {
         /// <summary>
-        /// Backing field for the <see cref="BreadCrumb"/> property
+        /// Backing field for the <see cref="BreadCrumb" /> property
         /// </summary>
         private string breadCrumb;
 
         /// <summary>
-        /// Backing field for the <see cref="Categories"/> property
+        /// Backing field for the <see cref="Categories" /> property
         /// </summary>
         private string categories;
 
         /// <summary>
-        /// Backing field for the <see cref="DefinitionContent"/> property
-        /// </summary>
-        private string definitionContent;
-
-        /// <summary>
-        /// the content of the <see cref="Definition"/> 
-        /// </summary>
-        private string originalDefinitionContent;
-
-        /// <summary>
-        /// Backing field for the <see cref="IsDirty"/> property
-        /// </summary>
-        private bool isDirty;
-
-        /// <summary>
-        /// Backing field for <see cref="EventPublisher"/>
-        /// </summary>
-        private EventPublisher eventPublisher;
-
-        /// <summary>
-        /// The <see cref="Definition"/> to display
+        /// The <see cref="Definition" /> to display
         /// </summary>
         private Definition definition;
 
         /// <summary>
-        /// The subscription on <see cref="definition"/>
+        /// Backing field for the <see cref="DefinitionContent" /> property
+        /// </summary>
+        private string definitionContent;
+
+        /// <summary>
+        /// The subscription on <see cref="definition" />
         /// </summary>
         private IDisposable definitionSubscription;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RequirementRowViewModel"/> class
+        /// Backing field for <see cref="EventPublisher" />
         /// </summary>
-        /// <param name="requirement">The <see cref="Requirement"/> associated with this row</param>
+        private EventPublisher eventPublisher;
+
+        /// <summary>
+        /// Backing field for the <see cref="IsDirty" /> property
+        /// </summary>
+        private bool isDirty;
+
+        /// <summary>
+        /// the content of the <see cref="Definition" />
+        /// </summary>
+        private string originalDefinitionContent;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequirementRowViewModel" /> class
+        /// </summary>
+        /// <param name="requirement">The <see cref="Requirement" /> associated with this row</param>
         /// <param name="session">The session</param>
-        /// <param name="containerViewModel">The <see cref="IViewModelBase{Thing}"/> that is the container of this <see cref="IRowViewModelBase{Thing}"/></param>
-        public RequirementRowViewModel(Requirement requirement, ISession session,
+        /// <param name="containerViewModel">
+        /// The <see cref="IViewModelBase{Thing}" /> that is the container of this
+        /// <see cref="IRowViewModelBase{Thing}" />
+        /// </param>
+        public RequirementRowViewModel(
+            Requirement requirement,
+            ISession session,
             IViewModelBase<Thing> containerViewModel)
             : base(requirement, session, containerViewModel)
         {
             this.InitializeCommands();
             this.EventPublisher = new EventPublisher();
 
+            this.CanEdit = this.Session.PermissionService.CanWrite(this.Thing);
+
             this.WhenAnyValue(vm => vm.DefinitionContent).Subscribe(
                 _ =>
                 {
                     var originalDefinition = this.Thing.Definition.FirstOrDefault();
-                    var originalDefinitionText = originalDefinition == null ? String.Empty : originalDefinition.Content;
+                    var originalDefinitionText = originalDefinition == null ? string.Empty : originalDefinition.Content;
                     this.IsDirty = this.DefinitionContent != originalDefinitionText;
                 });
 
@@ -91,52 +124,45 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// Gets the <see cref="EventPublisher"/>
+        /// Check the edit permission using <see cref="PermissionService" />
+        /// </summary>
+        public bool CanEdit { get; set; }
+
+        /// <summary>
+        /// Gets the <see cref="EventPublisher" />
         /// </summary>
         public EventPublisher EventPublisher
         {
-            get { return this.eventPublisher; }
-            private set { this.RaiseAndSetIfChanged(ref this.eventPublisher, value); }
+            get => this.eventPublisher;
+            private set => this.RaiseAndSetIfChanged(ref this.eventPublisher, value);
         }
 
         /// <summary>
-        /// Gets or sets the Save <see cref="ReactiveCommand"/> to save the content of the note.
+        /// Gets or sets the Save <see cref="ReactiveCommand" /> to save the content of the note.
         /// </summary>
         public ReactiveCommand<Unit> SaveCommand { get; protected set; }
 
         /// <summary>
-        /// Gets or sets the Cancel <see cref="ReactiveCommand"/> to save the content of the note.
+        /// Gets or sets the Cancel <see cref="ReactiveCommand" /> to save the content of the note.
         /// </summary>
         public ReactiveCommand<object> CancelCommand { get; protected set; }
 
         /// <summary>
-        /// Gets or sets the content of the <see cref="Definition"/> of the <see cref="Requirement"/>
+        /// Gets or sets the content of the <see cref="Definition" /> of the <see cref="Requirement" />
         /// </summary>
         public string DefinitionContent
         {
-            get { return this.definitionContent; }
-            set { this.RaiseAndSetIfChanged(ref this.definitionContent, value); }
+            get => this.definitionContent;
+            set => this.RaiseAndSetIfChanged(ref this.definitionContent, value);
         }
 
         /// <summary>
-        /// Gets or sets the <see cref="BreadCrumb"/> property
-        /// </summary>
-        /// <remarks>
-        /// The <see cref="BreadCrumb"/> property is used for sorting
-        /// </remarks>
-        public string BreadCrumb
-        {
-            get { return this.breadCrumb; }
-            set { this.RaiseAndSetIfChanged(ref this.breadCrumb, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the categories of the <see cref="Requirement"/> as comma separated short names
+        /// Gets or sets the categories of the <see cref="Requirement" /> as comma separated short names
         /// </summary>
         public string Categories
         {
-            get { return this.categories; }
-            set { this.RaiseAndSetIfChanged(ref this.categories, value); }
+            get => this.categories;
+            set => this.RaiseAndSetIfChanged(ref this.categories, value);
         }
 
         /// <summary>
@@ -144,13 +170,25 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         /// </summary>
         public bool IsDirty
         {
-            get { return this.isDirty; }
-            set { this.RaiseAndSetIfChanged(ref this.isDirty, value); }
+            get => this.isDirty;
+            set => this.RaiseAndSetIfChanged(ref this.isDirty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="BreadCrumb" /> property
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="BreadCrumb" /> property is used for sorting
+        /// </remarks>
+        public string BreadCrumb
+        {
+            get => this.breadCrumb;
+            set => this.RaiseAndSetIfChanged(ref this.breadCrumb, value);
         }
 
         /// <summary>
         /// The event-handler that is invoked by the subscription that listens for updates
-        /// on the <see cref="Thing"/> that is being represented by the view-model
+        /// on the <see cref="Thing" /> that is being represented by the view-model
         /// </summary>
         /// <param name="objectChange">
         /// The payload of the event that is being handled
@@ -193,9 +231,9 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// The <see cref="ObjectChangedEvent"/> handler for this <see cref="definition"/>
+        /// The <see cref="ObjectChangedEvent" /> handler for this <see cref="definition" />
         /// </summary>
-        /// <param name="defEvent">The <see cref="ObjectChangedEvent"/></param>
+        /// <param name="defEvent">The <see cref="ObjectChangedEvent" /></param>
         private void OnDefinitionUpdate(ObjectChangedEvent defEvent)
         {
             if (defEvent.EventKind == EventKind.Removed)
@@ -208,13 +246,14 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// Sets the value of the <see cref="DefinitionContent"/> property.
+        /// Sets the value of the <see cref="DefinitionContent" /> property.
         /// </summary>
         private void SetDefinitionContent()
         {
             if (this.definition == null)
             {
                 this.definition = this.Thing.Definition.FirstOrDefault();
+
                 if (this.definition == null)
                 {
                     return;
@@ -230,7 +269,7 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// Set the <see cref="Categories"/> property based on the <see cref="Requirement.Category"/> property
+        /// Set the <see cref="Categories" /> property based on the <see cref="Requirement.Category" /> property
         /// </summary>
         private void SetCategories()
         {
@@ -245,11 +284,12 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// Initializes the <see cref="ReactiveCommand"/>s.
+        /// Initializes the <see cref="ReactiveCommand" />s.
         /// </summary>
         private void InitializeCommands()
         {
             var canSave = this.WhenAnyValue(vm => vm.IsDirty).Select(_ => this.IsDirty);
+
             this.SaveCommand =
                 ReactiveCommand.CreateAsyncTask(canSave, x => this.WriteDefinitionUpdate(), RxApp.MainThreadScheduler);
 
@@ -258,7 +298,7 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// Resets the content to the content of the first <see cref="Definition"/> of the <see cref="Requirement"/>
+        /// Resets the content to the content of the first <see cref="Definition" /> of the <see cref="Requirement" />
         /// </summary>
         /// <param name="withWarning">
         /// If true, a warning message will be displayed.
@@ -268,10 +308,13 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
             // give a warning. If user cancels the changes are not reset.
             if (withWarning)
             {
-                var yesNoDialog = new YesNoDialogViewModel("Cancel Edit",
+                var yesNoDialog = new YesNoDialogViewModel(
+                    "Cancel Edit",
                     "Press Yes if you want to cancel the edit. All changes will be lost");
+
                 var dialogResult = this.dialogNavigationService.NavigateModal(yesNoDialog);
-                if (dialogResult != null && dialogResult.Result != null && dialogResult.Result.Value)
+
+                if ((dialogResult != null) && (dialogResult.Result != null) && dialogResult.Result.Value)
                 {
                     this.DefinitionContent = this.originalDefinitionContent;
                     this.EventPublisher.Publish(new ConfirmationEvent(true));
@@ -280,11 +323,12 @@ namespace CDP4Requirements.ViewModels.RequirementsSpecificationEditor
         }
 
         /// <summary>
-        /// Writes updates to the <see cref="Definition"/> to the data-soource
+        /// Writes updates to the <see cref="Definition" /> to the data-soource
         /// </summary>
         private async Task WriteDefinitionUpdate()
         {
             var definition = this.Thing.Definition.FirstOrDefault();
+
             if (definition != null)
             {
                 var definitionClone = definition.Clone(false);
