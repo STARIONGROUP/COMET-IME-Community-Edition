414bf03d81b93edd7b7108e45b6dd2b87de3ce33
jan-at-rhea <112615243+jan-at-rhea@users.noreply.github.com>
2022-09-09 09:30:00 +0200

    new TextBlocks

---
+++ CDP4Composition/CommonView/Items/AliasDisplayLayoutItem.xaml.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="AliasDisplayLayoutItem.xaml.cs" company="RHEA System S.A.">
+//   Copyright (c) 2015 RHEA System S.A.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace CDP4CommonView.Items
+{
+    using DevExpress.Xpf.LayoutControl;
+
+    /// <summary>
+    /// Interaction logic for AliasDisplayLayoutItem.xaml
+    /// </summary>
+    public partial class AliasDisplayLayoutItem : LayoutItem
+    {
+        public AliasDisplayLayoutItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CDP4Composition/CommonView/Items/DefinitionDisplayLayoutItem.xaml.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="DefinitionDisplayLayoutItem.xaml.cs" company="RHEA System S.A.">
+//   Copyright (c) 2015 RHEA System S.A.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace CDP4CommonView.Items
+{
+    using DevExpress.Xpf.LayoutControl;
+
+    /// <summary>
+    /// Interaction logic for DefinitionDisplayLayoutItem.xaml
+    /// </summary>
+    public partial class DefinitionDisplayLayoutItem : LayoutItem
+    {
+        public DefinitionDisplayLayoutItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
