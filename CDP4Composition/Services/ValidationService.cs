3fe578974f51e5e53dfb2f7b6abea2cf9ba71fe9
a.vandelft@rheagroup.com <a.vandelft@rheagroup.com>
2023-03-08 15:46:21 +0100

    Add tests and update headers

--- CDP4Composition/Services/ValidationService.cs
+++ CDP4Composition/Services/ValidationService.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ValidationService.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2022 RHEA System S.A.
+//    Copyright (c) 2015-2023 RHEA System S.A.
 // 
 //    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Antoine Théate, Omar Elebiary
 // 
