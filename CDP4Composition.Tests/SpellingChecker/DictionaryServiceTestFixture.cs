bb8301e4677bac634be0e1657715917b2279de09
Alexander van Delft <a.vandelft@rheagroup.com>
2024-05-07 09:59:48 +0200

    Fix unit tests

--- CDP4Composition.Tests/SpellingChecker/DictionaryServiceTestFixture.cs
+++ CDP4Composition.Tests/SpellingChecker/DictionaryServiceTestFixture.cs
@@ -26,7 +26,7 @@ namespace CDP4Composition.Tests.SpellingChecker
         public void SetUp()
         {
             var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var directoryInfo = Directory.CreateDirectory(folderPath + @"\STARION\CDP4\dictionaries\en-US");
+            var directoryInfo = Directory.CreateDirectory(folderPath + @"\STARION\CDP4-COMET\dictionaries\en-US");
             try
             {
                 var aff_file = Path.Combine(TestContext.CurrentContext.TestDirectory, @"SpellingChecker\en_US.aff");
@@ -54,7 +54,7 @@ namespace CDP4Composition.Tests.SpellingChecker
             var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             try
             {
-                Directory.Delete(folderPath + "\\STARION\\CDP4\\dictionaries", true);
+                Directory.Delete(folderPath + "\\STARION\\CDP4-COMET\\dictionaries", true);
             }
             catch (IOException ex)
             {
