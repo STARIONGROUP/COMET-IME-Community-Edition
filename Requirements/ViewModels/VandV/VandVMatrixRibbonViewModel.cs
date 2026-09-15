821237b6f180abd245b49a9a8ee7e0c9ae483004
Rowan de Voogt <r.devoogt@stariongroup.eu>
2026-09-15 12:19:50 +0200

    Fixed the Coveragematrix by making it a ribbon item

---
+++ Requirements/ViewModels/VandV/VandVMatrixRibbonViewModel.cs
@@ -0,0 +1,67 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VandVMatrixRibbonViewModel.cs" company="Starion Group S.A.">
+//    Copyright (c) 2015-2026 Starion Group S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Antoine Théate, Rowan de Voogt
+//
+//    This file is part of CDP4-COMET IME Community Edition.
+//    The CDP4-COMET IME Community Edition is the Starion Concurrent Design Desktop Application and Excel Integration
+//    compliant with ECSS-E-TM-10-25 Annex A and Annex C.
+//
+//    The CDP4-COMET IME Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or any later version.
+//
+//    The CDP4-COMET IME Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program. If not, see http://www.gnu.org/licenses/.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Requirements.ViewModels
+{
+    using CDP4Common.EngineeringModelData;
+
+    using CDP4Composition.Mvvm;
+    using CDP4Composition.Navigation;
+    using CDP4Composition.Navigation.Interfaces;
+    using CDP4Composition.PluginSettingService;
+
+    using CDP4Dal;
+
+    /// <summary>
+    /// The view-model for the ribbon control that opens the VCRM (V&amp;V Coverage Matrix) for an open
+    /// <see cref="Iteration"/>.
+    /// </summary>
+    public class VandVMatrixRibbonViewModel : RibbonButtonIterationDependentViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VandVMatrixRibbonViewModel"/> class.
+        /// </summary>
+        /// <param name="messageBus">The <see cref="ICDPMessageBus"/>.</param>
+        public VandVMatrixRibbonViewModel(ICDPMessageBus messageBus)
+            : base(InstantiatePanelViewModel, messageBus)
+        {
+        }
+
+        /// <summary>
+        /// Returns an instance of the <see cref="VandVMatrixViewModel"/> for the supplied <see cref="Iteration"/>.
+        /// </summary>
+        /// <param name="iteration">The <see cref="Iteration"/>.</param>
+        /// <param name="session">The <see cref="ISession"/>.</param>
+        /// <param name="thingDialogNavigationService">The <see cref="IThingDialogNavigationService"/>.</param>
+        /// <param name="panelNavigationService">The <see cref="IPanelNavigationService"/>.</param>
+        /// <param name="dialogNavigationService">The <see cref="IDialogNavigationService"/>.</param>
+        /// <param name="pluginSettingsService">The <see cref="IPluginSettingsService"/>.</param>
+        /// <returns>An instance of <see cref="VandVMatrixViewModel"/>.</returns>
+        public static VandVMatrixViewModel InstantiatePanelViewModel(Iteration iteration, ISession session, IThingDialogNavigationService thingDialogNavigationService, IPanelNavigationService panelNavigationService, IDialogNavigationService dialogNavigationService, IPluginSettingsService pluginSettingsService)
+        {
+            return new VandVMatrixViewModel(iteration, session, thingDialogNavigationService, panelNavigationService, dialogNavigationService, pluginSettingsService);
+        }
+    }
+}
